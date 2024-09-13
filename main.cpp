58ceb60864ee7fc21e197a46e1ce14761afa6559
Chocolate <artemnes1j@gmail.com>
2024-09-13 23:08:49 +0300

    function to run reading, sorting and writing results

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "colors.h"
 #include "onegin.h"
 
+static exit_code_t run_program(text_t *text);
+
 int main(const int argc, const char *argv[]) {
     text_t text = {};
     switch(parse_input(&text, argc, argv)) {
@@ -16,27 +18,34 @@ int main(const int argc, const char *argv[]) {
             return EXIT_FAILURE;
     }
 
-    if(      try_read_file(&text) != EXIT_CODE_SUCCESS) {
-        free_text(&text);
+    if(run_program(&text) != EXIT_CODE_SUCCESS)
         return EXIT_FAILURE;
+
+    return EXIT_SUCCESS;
+}
+
+exit_code_t run_program(text_t *text) {
+    if(      try_read_file(text) != EXIT_CODE_SUCCESS) {
+        free_text(text);
+        return EXIT_CODE_FAILURE;
     }
-    if(     try_parse_text(&text) != EXIT_CODE_SUCCESS) {
-        free_text(&text);
-        return EXIT_FAILURE;
+    if(     try_parse_text(text) != EXIT_CODE_SUCCESS) {
+        free_text(text);
+        return EXIT_CODE_FAILURE;
     }
-    if(try_sort_alphabetic(&text) != EXIT_CODE_SUCCESS) {
-        free_text(&text);
-        return EXIT_FAILURE;
+    if(try_sort_alphabetic(text) != EXIT_CODE_SUCCESS) {
+        free_text(text);
+        return EXIT_CODE_FAILURE;
     }
-    if(     try_sort_rhyme(&text) != EXIT_CODE_SUCCESS) {
-        free_text(&text);
-        return EXIT_FAILURE;
+    if(     try_sort_rhyme(text) != EXIT_CODE_SUCCESS) {
+        free_text(text);
+        return EXIT_CODE_FAILURE;
     }
-    if( try_print_original(&text) != EXIT_CODE_SUCCESS) {
-        free_text(&text);
-        return EXIT_FAILURE;
+    if( try_print_original(text) != EXIT_CODE_SUCCESS) {
+        free_text(text);
+        return EXIT_CODE_FAILURE;
     }
 
-    free_text(&text);
-    return EXIT_SUCCESS;
+    free_text(text);
+    return EXIT_CODE_SUCCESS;
 }
